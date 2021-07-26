ba4d0d492914225e2656310b9b3b320f6896c945
Aquaman <aquaman@digitalheroes.tech>
2021-07-27 00:09:13 +0300

    show results on user interface

--- ClientAppSwift/ViewController.swift
+++ ClientAppSwift/ViewController.swift
@@ -21,39 +21,79 @@ class ViewController: UIViewController {
     }
     
     @objc private func startTextRecognition() {
-        VerSDK.shared.textRecognition { result in
+        VerSDK.shared.textRecognition { [weak self] result in
+            guard let self = self else { return }
             switch result {
-            case .success(let prediction):
-                print(prediction)
+            case .success(let recognition):
+                self.showTextRecognition(recognition.joined(separator: " "))
             case .failure(let error):
-                print(error.localizedDescription)
+                self.showError(error)
             }
         }
     }
     
     @objc private func startFaceDetection() {
-        VerSDK.shared.faceDetection() { result in
+        VerSDK.shared.faceDetection() { [weak self] result in
+            guard let self = self else { return }
             switch result {
-            case .success(let prediction):
-                print(prediction)
+            case .success(let image):
+                self.showFaceImage(image)
             case .failure(let error):
-                print(error.localizedDescription)
+                self.showError(error)
             }
         }
     }
     
-    let textRecognitionButton: UIButton = {
+    private func showTextRecognition(_ text: String) {
+        showAlert(NSLocalizedString("Text recognition result", comment: ""), text, nil)
+    }
+    
+    private func showFaceImage(_ image: UIImage) {
+        showAlert(NSLocalizedString("Face detection result", comment: ""), "", image)
+    }
+    
+    private func showError(_ error: Error) {
+        showAlert(NSLocalizedString("Error", comment: ""), error.localizedDescription, nil)
+    }
+    
+    private func showAlert(_ title: String, _ message: String, _ image: UIImage?) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .actionSheet)
+        if let image = image {
+            let scaledSize = CGSize(width: image.size.width / 3, height: image.size.height / 3)
+            let scaledImage = image.imageWithSize(scaledToSize: scaledSize)
+            let action = UIAlertAction(title: "", style: .default, handler: nil)
+            action.setValue(scaledImage.withRenderingMode(.alwaysOriginal), forKey: "image")
+            alert.addAction(action)
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private let textRecognitionButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Start Text Recognition", for: .normal)
         button.addTarget(self, action: #selector(startTextRecognition), for: .touchUpInside)
         return button
     }()
     
-    let faceDetectionButton: UIButton = {
+    private let faceDetectionButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Start Face Detection", for: .normal)
         button.addTarget(self, action: #selector(startFaceDetection), for: .touchUpInside)
         return button
     }()
+}
+
+extension UIImage {
+    // Scale image for UIAlertAction
+    func imageWithSize(scaledToSize newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
 
